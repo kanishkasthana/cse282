1c5225032228ba41003c88d400fa36386317a74b
Kanishk Asthana <kani.asthana@gmail.com>
2015-01-14 00:58:03 -0800

    The First Commit

---
+++ src/bioinformatics/Bioinformatics.java
@@ -0,0 +1,342 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bioinformatics;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Kanishk Asthana kasthana@eng.ucsd.edu
+ */
+public class Bioinformatics {
+
+    private String text=null;
+    private List frequentPatterns=null;
+    int[] frequencyArray=null;
+    private String reverseText=null;
+    
+    public Bioinformatics(String text){
+        this.text=text;
+        
+    }
+    
+    public void setText(String text){
+        this.text=text;
+    }
+    
+    public String getText(){
+        return this.text;
+        
+    }
+    
+    public String getReverseText(){
+        this.reverseText();
+        return this.reverseText;
+    }
+    public List getFrequentWords(){
+     return this.frequentPatterns;
+    }
+    
+    public int[] getFrequencyArray(){
+     return frequencyArray;
+    }
+    
+    public int patternCount(String pattern){
+        int count=0,i;
+        int k=pattern.length(),l=text.length();
+        for(i=0;i<=(l-k);i++)
+        {  
+         if(text.substring(i,i+k).equals(pattern))
+             count++;
+        }
+        return count;
+    }
+    
+    public void frequentWords(int k){
+    
+        List frequentPatterns= new ArrayList();
+        int[] count= new int[text.length()-k+1];
+        int l=text.length(),i;
+        String pattern;
+        
+        for(i=0;i<=(l-k);i++){
+         pattern=text.substring(i,i+k);
+         count[i]=patternCount(pattern);
+        }
+        //Storing Maximum Value from Count Array
+        int maxCount=maximum(count);
+        
+        for(i=0;i<=(l-k);i++){
+         if(count[i]==maxCount){
+          frequentPatterns.add(text.substring(i,i+k));
+         }
+        }
+        this.frequentPatterns=frequentPatterns;
+        this.removeDuplicates();
+    }
+    
+    public int maximum(int[] array){
+     int max=array[0];
+     for(int i=1;i<array.length;i++){
+         if(array[i]>max){
+         max=array[i];
+        }
+    
+     }  
+     return max;
+    }
+    public void removeDuplicates(){
+     
+    List uniqueElements=new ArrayList(); 
+    Object tempPattern;
+    int present;
+        for (Object pattern : frequentPatterns) {
+            present=0;
+            tempPattern=pattern;
+            for (Object pattern1 : uniqueElements) {
+              if(pattern1.equals(tempPattern))
+              {
+               present=1;
+               break;
+              }
+            }
+            if(present==0)
+                uniqueElements.add(tempPattern);
+        }
+        
+        this.frequentPatterns=uniqueElements;
+    }
+    
+    public int symbolToNumber(char symbol){
+     int number;
+     //Lexically arranged in increasing order
+     switch(symbol){
+         case 'A':
+             number=0;
+             break;
+         case 'C':
+             number=1;
+             break;
+         case 'G':
+             number=2;
+             break;
+         case 'T':
+             number=3;
+             break;
+         default:
+             number=0;
+             break;
+     }
+     return number;   
+    }
+    
+    public char numberToSymbol(int number){
+     char symbol;
+     switch(number){
+         case 0:
+             symbol='A';
+             break;
+         case 1:
+             symbol='C';
+             break;
+         case 2:
+             symbol='G';
+             break;
+         case 3:
+             symbol='T';
+             break;
+         default:
+             symbol='A';
+             break;
+     }
+     return symbol;
+    }
+    
+    public int patternToNumber(String pattern){
+     char symbol;   
+     if(pattern.isEmpty())
+         return 0;
+     else{
+      symbol=pattern.charAt(pattern.length()-1);
+      pattern=pattern.substring(0,pattern.length()-1);
+      return 4*patternToNumber(pattern)+ symbolToNumber(symbol);
+     }
+    }
+    
+    public String numberToPattern(int index,int k){
+     if(k==1)
+         return numberToSymbol(index)+"";//Converting character to string
+     int prefixIndex=quotient(index,4);
+     int r=remainder(index,4);
+     String prefixPattern=numberToPattern(prefixIndex,k-1);
+     char symbol=numberToSymbol(r);
+     //Returning Concatenation;
+     return prefixPattern+symbol;
+    }
+    
+    public int quotient(int dividend, int divisor){
+     return dividend/divisor;
+    }
+    
+    public int remainder(int dividend, int divisor){
+     return dividend%divisor;
+    }
+    
+    public int power(int base,int exponent){
+     int answer=1;
+     for(int i=0;i<exponent;i++){
+      answer=answer*base;
+     }
+     return answer;
+    }
+    
+    public int[] computingFrequencies(int k){
+     int end=power(4,k),i,j;
+     int[] frequencyArray= new int[end];
+     String pattern;
+     
+     for(i=0;i<=end-1;i++){
+      frequencyArray[i]=0;
+     }
+     
+     for(i=0;i<=text.length()-k;i++){
+      pattern=text.substring(i,i+k);
+      j=patternToNumber(pattern);
+      frequencyArray[j]++;
+     }
+       this.frequencyArray=frequencyArray;
+       return frequencyArray;
+    }
+    
+    public void fasterFrequentWords(int k){
+     List frequentPatterns=new ArrayList();
+     computingFrequencies(k);
+     int maxCount=maximum(this.frequencyArray);
+     int end=power(4,k),i;
+     String pattern;
+     
+     for(i=0;i<=end-1;i++){
+         if(frequencyArray[i]==maxCount){
+          pattern=numberToPattern(i,k);
+          frequentPatterns.add(pattern);
+         }
+     }
+      this.frequentPatterns=frequentPatterns;
+    }
+    
+    public void findingFrequentWordsBySorting(int k){
+     List frequentPatterns=new ArrayList();
+     int[] count=new int[text.length()-k+1];
+     int[] index=new int[text.length()-k+1];
+     int[] sortedIndex=new int[text.length()-k+1];
+     int i,maxCount;
+     String pattern;
+     for(i=0;i<=text.length()-k;i++){
+      pattern=text.substring(i,i+k);
+      index[i]=patternToNumber(pattern);
+      count[i]=1;
+     }
+     sortedIndex=sort(index);
+     
+     for(i=1;i<=text.length()-k;i++){
+         if(sortedIndex[i]==sortedIndex[i-1])
+             count[i]=count[i-1]+1;
+     }
+     maxCount=maximum(count);
+     
+     for(i=0;i<=text.length()-k;i++){
+         if(count[i]==maxCount){
+             pattern=numberToPattern(sortedIndex[i],k);
+             frequentPatterns.add(pattern);
+         }            
+     }
+     this.frequentPatterns=frequentPatterns;
+    }
+    
+    //Implementing bubble sort
+    public int[] sort(int[] array)
+    {
+    boolean swapped = true;
+    int j = 0,temp;
+    while (swapped) {
+        swapped = false;
+        j++;
+        for (int i = 0; i < array.length - j; i++) {
+            if (array[i] > array[i + 1]) {
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+     }
+     return array;
+    }
+    
+    public void reverseText(){
+     reverseText=reverseComplement(text);
+    }
+    public static String reverseComplement(String forwardStrand){
+        StringBuilder reverseStrand=new StringBuilder();
+        char base;
+        for(int i=forwardStrand.length()-1;i>=0;i--){
+           base=forwardStrand.charAt(i);
+           reverseStrand.append(complementaryBase(base));
+        }
+        return reverseStrand.toString();
+    }
+    
+    public static char complementaryBase(char base){
+     switch(base){
+         case 'A': return 'T';
+         case 'T': return 'A';
+         case 'G': return 'C';
+         case 'C': return 'G';
+         default: return 'A';
+     }
+    }
+    
+    public String patternMatching(String pattern){
+     StringBuilder positions=new StringBuilder();
+     
+     return positions.toString();
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+            //Getting Working Directory
+            System.out.println(System.getProperty("user.dir"));
+            //Storing inputs in list inputs
+            List inputs= new ArrayList();
+            //Reading downloaded file
+            File newFile=new File("rosalind_1b.txt");
+            FileReader fileReader=new FileReader(newFile);
+            BufferedReader reader=new BufferedReader(fileReader);
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+             inputs.add(line);
+            }
+            //Creating PrintWriter for writing to output file
+            PrintWriter out= new PrintWriter(new FileWriter("out.txt"));
+            //Creating new Object to handle this string
+            Bioinformatics newText=new Bioinformatics(inputs.get(1).toString());
+            String pattern=inputs.get(0).toString();
+            out.print(newText.patternMatching(pattern));
+            
+            out.close();
+            
+        }
+        catch(Exception e)
+        {
+         e.printStackTrace();
+        }
+        
+    }
+    
+}
